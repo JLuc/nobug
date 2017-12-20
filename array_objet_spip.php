de98e8c3230b6b3c12cb58573e65cf3d73dbab30
JLuc <jluc@no-log.org>
2017-12-20 12:34:12 +0100

    example pour contrainte non élémentaire

--- array_objet_spip.php
+++ array_objet_spip.php
@@ -3,16 +3,18 @@
 // Exemple de fonction spécialisée pour décrire une contrainte non élémentaire
 //
 
-// vérifier que $a est un array de la forme ('id_objet' => 9, 'objet' = > "article")
+// s'assurer que $a est un array de la forme ('id_objet' => 9, 'objet' = > "article")
 // $ctx est un contexte optionnel permettant d'éclaircir le contexte
 // et de documenter la contrainte
 function nobug_array_objet_spip ($a, $ctx='') {
-  nobug_assert (is_array($a), "$ctx : pas un tableau");
-  nobug_assert (count($a)==2, "$ctx : le tableau n'a pas le bon nombre d'éléments");
-  nobug_assert (isset($a['objet']), "$ctx : pas de clé objet");
-  nobug_assert (isset($a['id_objet']), "$ctx : pas de clé id_objet");
-  nobug_assert (isnumeric ($a['id_objet']), "$ctx : l'id_objet n'est pas numérique");
-  // éventuellement : 
+  ( nobug_assert (is_array($a), "$ctx : pas un tableau") 
+   and  nobug_assert (count($a)==2, "$ctx : le tableau devrait avoir 2 éléments mais en a ".count($a)) 
+   and  nobug_assert (isset($a['objet']), "$ctx : pas de clé objet") 
+   and  nobug_assert (isset($a['id_objet']), "$ctx : pas de clé id_objet") 
+   and  nobug_assert (isnumeric ($a['id_objet']), "$ctx : l'id_objet n'est pas numérique")
+  // éventuellement encore : 
   // tester que $a['objet'] est bien une table spip déclarée
-  // tester que l'objet $id_objet existe bien...
+  // tester que l'objet $id_objet existe bien...  
+  )
+    or nobug_assert (false, "Valeur actuelle : print_r($a,1));
 }
